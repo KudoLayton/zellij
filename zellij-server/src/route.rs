e23be6141ef844b72f511f822d458e756f0361d5
KudoLayton <lhslhg@gmail.com>
2025-09-10 20:31:01 +0900

    feat: squash all windows build patch
    ^ Conflicts:
    ^	zellij-server/Cargo.toml
    
    ^ Conflicts:
    ^	rust-toolchain.toml
    
    ^ Conflicts:
    ^	Cargo.lock
    
    ^ Conflicts:
    ^	zellij-utils/src/data.rs
    
    ^ Conflicts:
    ^	Cargo.lock
    
    ^ Conflicts:
    ^	zellij-server/src/terminal_bytes.rs
    
    ^ Conflicts:
    ^	zellij-server/Cargo.toml
    
    ^ Conflicts:
    ^	Cargo.lock
    
    ^ Conflicts:
    ^	zellij-server/Cargo.toml

--- zellij-server/src/route.rs
+++ zellij-server/src/route.rs
@@ -981,6 +981,10 @@ pub(crate) fn route_thread_main(
     let err_context = || format!("failed to handle instruction for client {client_id}");
     let mut seen_cli_pipes = HashSet::new();
     'route_loop: loop {
+        match receiver.is_usable() {
+            Ok(true) => {},
+            _ => break 'route_loop,
+        }
         match receiver.recv() {
             Some((instruction, err_ctx)) => {
                 err_ctx.update_thread_ctx();
@@ -989,6 +993,7 @@ pub(crate) fn route_thread_main(
                     &mut VecDeque<ClientToServerMsg>,
                 >|
                  -> Result<bool> {
+                    log::debug!("Processing message {}", instruction);
                     let mut should_break = false;
                     let rlocked_sessions =
                         session_data.read().to_anyhow().with_context(err_context)?;
@@ -1070,22 +1075,23 @@ pub(crate) fn route_thread_main(
                                 .to_anyhow()
                                 .with_context(err_context)?
                                 .set_client_size(client_id, new_size);
-                            session_state
-                                .read()
-                                .to_anyhow()
-                                .and_then(|state| {
-                                    state.min_client_terminal_size().ok_or(anyhow!(
-                                        "failed to determine minimal client terminal size"
-                                    ))
-                                })
-                                .and_then(|min_size| {
-                                    rlocked_sessions
-                                        .as_ref()
-                                        .context("couldn't get reference to read-locked session")?
-                                        .senders
-                                        .send_to_screen(ScreenInstruction::TerminalResize(min_size))
-                                })
-                                .with_context(err_context)?;
+
+                            if let Some(rlocked_sessions) = rlocked_sessions.as_ref() {
+                                session_state
+                                    .read()
+                                    .to_anyhow()
+                                    .and_then(|state| {
+                                        state.min_client_terminal_size().ok_or(anyhow!(
+                                            "failed to determine minimal client terminal size"
+                                        ))
+                                    })
+                                    .and_then(|min_size| {
+                                        rlocked_sessions.senders.send_to_screen(
+                                            ScreenInstruction::TerminalResize(min_size),
+                                        )
+                                    })
+                                    .with_context(err_context)?;
+                            }
                         },
                         ClientToServerMsg::TerminalPixelDimensions(pixel_dimensions) => {
                             send_to_screen_or_retry_queue!(
@@ -1208,18 +1214,33 @@ pub(crate) fn route_thread_main(
                     let should_break =
                         handle_instruction(instruction_to_retry, Some(&mut repeat_retries))?;
                     if should_break {
+                        #[cfg(windows)]
+                        {
+                            match receiver.clear_read_buffer() {
+                                Ok(()) => {},
+                                Err(e) => log::error!("Error ocurred while flushing remaining read buffer on server router: {}", e)
+                            }
+                        }
                         break 'route_loop;
                     }
                 }
+
                 // retry on loop around
                 retry_queue.append(&mut repeat_retries);
-                let should_break = handle_instruction(instruction, Some(&mut retry_queue))?;
+                let mut should_break = handle_instruction(instruction, Some(&mut retry_queue))?;
                 if should_break {
+                    #[cfg(windows)]
+                    {
+                        match receiver.clear_read_buffer() {
+                            Ok(()) => {},
+                            Err(e) => log::error!("Error ocurred while flushing remaining read buffer on server router: {}", e)
+                        }
+                    }
                     break 'route_loop;
                 }
             },
             None => {
-                log::error!("Received empty message from client, logging client out.");
+                log::error!("Error receiving Message from client, logging client out.");
                 let _ = os_input.send_to_client(
                     client_id,
                     ServerToClientMsg::Exit(ExitReason::Error(
@@ -1231,5 +1252,6 @@ pub(crate) fn route_thread_main(
             },
         }
     }
+    log::info!("Terminating server_router");
     Ok(())
 }
